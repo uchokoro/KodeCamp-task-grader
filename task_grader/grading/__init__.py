8abfb7286eb3bd66ff732f4c1c1d8b821114f22f
uchokoro <antoine.okoro@gmail.com>
2026-04-20 15:50:03 +0100

    refactor(grading): expose sub-module members in `__init__.py`

---
+++ task_grader/grading/__init__.py
@@ -0,0 +1,4 @@
+from .evaluator import EvaluationResult, LLMTaskEvaluator
+from .rubric import Criterion, Rubric, ScoreScale
+
+__all__ = ["Criterion", "EvaluationResult", "LLMTaskEvaluator", "Rubric", "ScoreScale"]
